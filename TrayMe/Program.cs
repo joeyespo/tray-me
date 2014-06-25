c23be83045ae9103a2be0d8382af5aa1d44dd70e
Joe Esposito <joe@joeyespo.com>
2014-06-24 22:02:52 -0400

    Comments.

--- TrayMe/Program.cs
+++ TrayMe/Program.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace TrayMe
 {
     /// <summary>
-    /// Represents a ...
+    /// Represents the TrayMe application.
     /// </summary>
     internal static class Program
     {
